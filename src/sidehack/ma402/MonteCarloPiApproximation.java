d4c07e8f5909e141ecc03846911d1edac88e9dc9
Nkubitu <muthonielaine@gmail.com>
2016-01-24 01:18:48 -0500

    Update MonteCarloPiApproximation.java
    Changed return type to double since we want to return approximate values, then cast int n to a double to get a double after dividing pointsInCircle with n.

--- src/sidehack/ma402/MonteCarloPiApproximation.java
+++ src/sidehack/ma402/MonteCarloPiApproximation.java
@@ -2,49 +2,39 @@ package sidehack.ma402;
 
 import java.util.Scanner;
 
-public class MonteCarloPiApproximation {
+	public class piMonte {
 
-	/**
-	 * Pi Approximations using Monte Carlo approximation. Using a square and a
-	 * quarter a circle, then using
-	 * 
-	 * @author CarlisNdemo
-	 * 
-	 */
-	public static void main(String[] args) {
+		public static void main(String args[]) {
 
-		@SuppressWarnings("resource")
-		Scanner totalPoints = new Scanner(System.in);
+			@SuppressWarnings("resource")
+			Scanner totalPoints = new Scanner(System.in);
 
-		System.out.println("Enter the number of points for approximation: ");
-		int numPoints = totalPoints.nextInt();
+			System.out.println("Enter the number of points for approximation: ");
+			int numPoints = totalPoints.nextInt();
 
-		System.out.println("Your Pi Approximation was: " + valueOfPi(numPoints));
+			System.out.println("Your Pi Approximation was: " + valueOfPi(numPoints));
 
-	}
+		}
+		public static double valueOfPi(int n) {
 
-	// Generates value of Pi using Monte Carlo simulation.
-	public static int valueOfPi(int n) {
+			int pointsInCircle = 0;
 
-		int pointsInCircle = 0;
-
-		for (int i = 0; i < n; i++) {
-
-			double x = Math.random();
-			double y = Math.random();
-
-			//System.out.println(x + " , " + y);
-
-			if (((x * x) + (y * y)) <= 1.00) {
-				pointsInCircle++;
-			} else {
-				continue;
+			for (int i = 0; i < n; i++) {
+				double x = Math.random();
+				double y = Math.random();
+				//System.out.println(x + " , " + y);
+				if (((x * x) + (y * y)) <= 1.00) {
+					pointsInCircle++;
+				} 
+				else {
+					continue;
+				}
 			}
-
-		} // ends for loop
-
-		int pI = 4 * (pointsInCircle / n);
-		return pI;
+			double pI = 4 * (pointsInCircle /(double) n);
+			return pI;	
+		}		
 	}
-}
+
+
+
 
